7fad9dc6d4c3872e5874f9c9985ce85388729357
Craig Reay <craigcsruk@googlemail.com>
2015-10-02 20:18:54 +0100

    added example get, put, post to help out with tomorrow.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,37 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var fs = require('fs')
+
+router.get('/books', function(req, res){
+  var filename = process.argv[3]
+  fs.readFile(filename, function(e, data) {
+    if (e) return res.sendStatus(500)
+    try {
+      books = JSON.parse(data)
+    } catch (e) {
+      res.sendStatus(500)
+    }
+    res.json(books)
+  })
+})
+
+router.put('/message/:id', function(req, res){
+  var id = req.params.id
+  var str = require('crypto')
+    .createHash('sha1')
+    .update(new Date().toDateString() + id)
+    .digest('hex')
+  res.send(str)
+})
+
+router.get('/search', function(req, res){
+  var query = req.query
+  res.send(query)
+})
+
+router.post('/form', function(req, res) {
+  res.send(req.body.str.split('').reverse().join(''))
+})
+
 module.exports = router;
